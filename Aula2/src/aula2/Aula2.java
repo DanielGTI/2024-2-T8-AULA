47cb1724cedeb15c50ac4350cd208e92dca34ce1
Daniel Ferreira de Barros Junior <daniel.fb.jr@gmail.com>
2024-10-17 11:19:55 -0300

    Update Aula2.java

--- Aula2/src/aula2/Aula2.java
+++ Aula2/src/aula2/Aula2.java
@@ -1,17 +1,31 @@
 package aula2;
-
-
+import java.util.*;
 public class Aula2 {
-
-
     public static void main(String[] args) {
-        Aluno aluno1 = new Aluno(20241234, 7.0, 9.0);
         
+        Scanner leia = new Scanner(System.in);
+        Random gerar = new Random();
+        Turma turma = new Turma();
+        double ra, av1, av2;
+        
+        // Gerar dados dos alunos
+        for( int i=0; i<10; i++){
+            ra = (gerar.nextInt(1000)) + 240000;
+            av1 = gerar.nextInt(11);
+            av2 = gerar.nextInt(11);
+            turma.adicionarAluno(new Aluno(ra, av1, av2));
+        }      
+        //  EXIBIR TURMA
+        turma.exibirTurma();
+        
+        
+        /*
+        Aluno aluno1 = new Aluno(20241234, 3.0, 9.0);
         aluno1.exibirInformacoes();
-        
         aluno1.setRa(2000123);
-        
-        System.out.printf("RA: %.0f\nAV1: %.2f\nAV2: %.2f\n", aluno1.getRa(), aluno1.av1, aluno1.av2);
+        System.out.printf("RA: %.0f\nAV1: %.2f\nAV2: %.2f\n", 
+                aluno1.getRa(), aluno1.getAv1(), aluno1.av2);
+        */
     }
     
 }
