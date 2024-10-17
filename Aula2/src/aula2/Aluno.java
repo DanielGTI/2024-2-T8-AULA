8f227577832b615cd92ea358d5be41858d37d50d
Daniel Ferreira de Barros Junior <daniel.fb.jr@gmail.com>
2024-10-17 11:19:29 -0300

    Update Aluno.java

--- Aula2/src/aula2/Aluno.java
+++ Aula2/src/aula2/Aluno.java
@@ -1,14 +1,11 @@
 package aula2;
-
-public class Aluno {
-    
+public class Aluno {   
     //  ATRIBUTOS
     private double ra;  // PRIVADO
-    public double av1;  // PUBLICO
+    private double av1;  // PRIVADO
     public double av2;  // PUBLICO
     
     //  MÉTODO CONSTRUTOR
-
     public Aluno(double ra, double av1, double av2) {
         this.ra = ra;
         this.av1 = av1;
@@ -19,15 +16,26 @@ public class Aluno {
     public double getRa() {
         return ra;
     }
-
+    //  SETTER = MÉTODO MODIFICADOR
     public void setRa(double ra) {
         this.ra = ra;
     }
     
+    public double getAv1(){
+        if( this.av1 < 5.0 )
+            return 0.0;
+        else
+            return av1;
+    }
+    
+    public void setAv1(double av1){
+        this.av1 = av1;
+    }
     
     
     public void exibirInformacoes(){
-        System.out.printf("%.0f\t %.2f\t %.2f\t\n", ra, av1, av2);
+        System.out.printf("%.0f\t %.2f\t %.2f\t\n",
+                ra, av1, av2);
     }
     
 }
